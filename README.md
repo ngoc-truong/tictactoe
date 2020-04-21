d4b43f0c984f0dd84aef0cacafd79184546727cd
Ngoc Truong <ngoc.truong@posteo.de>
2020-04-21 18:33:58 +0200

    Finish tic tac toe game

---
+++ README.md
@@ -0,0 +1,4 @@
+= Tic Tac Toe
+
+This is a beginner javascript recap project from [The Odin Project](https://www.theodinproject.com/courses/web-development-101/lessons/rock-paper-scissors). 
+The goal is to build a simple Tic Tac Toe game in Javascript to play in the browser console. 
---
+++ main.js
@@ -0,0 +1,49 @@
+function computerPlay(){
+    let sign = ["rock", "paper", "scissors"];
+    let choice = Math.floor(Math.random() * 3);
+
+    return sign[choice];
+}
+
+function playerPlay(){
+    let selection = prompt("Please select rock, paper or scissors?");
+    selection = selection.toLowerCase();
+
+    while (selection !== "rock" && selection !== "paper" && selection !== "scissors"){
+        selection = prompt("Sorry, man, but that is not a valid choice! Choose rock, paper or scissors.");
+        selection = selection.toLowerCase();
+    }
+
+    return selection;
+}
+
+function oneRound(playerSelection, computerSelection){
+
+    if (playerSelection === computerSelection) {
+        return `You both chose ${playerSelection}. It's a draw`;
+    } else if ( playerSelection === "rock" && computerSelection === "scissors" ||
+                playerSelection === "paper" && computerSelection === "rock" ||
+                playerSelection === "scissors" && computerSelection === "paper") {
+
+        return `You won! ${capitalize(playerSelection)} beats ${computerSelection} :).`;
+    } else {
+        return `Sorry, you lose! ${capitalize(playerSelection)} is beaten by ${computerSelection} :(.`;
+    }
+}
+
+function capitalize(word) {
+    return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+function game(){
+    for (i = 1; i <= 5; i++) {
+        console.log(`Round ${i}`);
+        let computer = computerPlay();
+        let human = playerPlay();
+    
+        let result = oneRound(human, computer);
+        console.log(result);
+    }
+}
+
+game();
